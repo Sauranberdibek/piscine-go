fffe44dce174be68db8915bd6f7208bcb1ccffd1
Sauranberdibek <ms.sauran@gmail.com>
2019-10-16 15:23:00 +0600

    Prit1

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,12 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func main() { 
+    for i := 97 ; i <=122 ; i++ { 
+    
+        z01.PrintRune(rune(i))
+}
 
-
-    fmt.Println("abcdefghijklmnopqrstuvwxyz \n")
-
-
-
+    z01.PrintRune('\n')
+    
 }
