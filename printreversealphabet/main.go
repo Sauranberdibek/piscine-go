63ff7cb88170c2db386b38540ae6fa234976c9aa
Sauranberdibek <ms.sauran@gmail.com>
2019-10-16 16:43:25 +0600

    ddd2

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,9 +3,10 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for i := 122; i >= 97; i-- {
+	for i := 'z'; i >= 'a'; i--
 
-		z01.PrintRune(rune(i))
+	{
+		z01.PrintRune(i)
 	}
 
 	z01.PrintRune('\n')
