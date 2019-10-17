b06cde5c1d3d018d02a6c22bd42086141065bf2a
Sauranberdibek <ms.sauran@gmail.com>
2019-10-17 19:55:27 +0600

    ggg8

--- strrev.go
+++ strrev.go
@@ -2,7 +2,11 @@ package piscine
 
 func StrRev(s string) string {
 	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+	counter := 0 
+	for range s {
+		counter++ 
+	}
+	for i, j := 0, counter ; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
