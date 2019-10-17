2fa3ccf3fd51880283b5835a6a49f472f853c459
Sauranberdibek <ms.sauran@gmail.com>
2019-10-17 19:56:48 +0600

    ggg8

--- strrev.go
+++ strrev.go
@@ -4,9 +4,9 @@ func StrRev(s string) string {
 	runes := []rune(s)
 	counter := 0 
 	for range s {
-		counter++ 
+		counter++
 	}
-	for i, j := 0, counter ; i < j; i, j = i+1, j-1 {
+	for i, j := 0, counter; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
