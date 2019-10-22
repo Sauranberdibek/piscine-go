d7011b50e8ad5c5578eb0506348d96db05051e02
Sauranberdibek <ms.sauran@gmail.com>
2019-10-22 21:01:00 +0600

    quest5-5

--- nrune.go
+++ nrune.go
@@ -1,7 +1,15 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	tr := []rune(s)
-	return (tr[n-1])
+	ln := 0
+	for i := range s{
+		ln = int
+	}
 
+	tr := []rune(s)
+	if ln >= n{
+	return (tr[n-1])
+	} else {
+		return 0
+	}
 }
