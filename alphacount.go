999eae29857e5fbdd5e9ed9f0d3267ca9f417af3
Sauranberdibek <ms.sauran@gmail.com>
2019-10-22 17:29:38 +0600

    quest05-al

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(str string) int {
 	count := 0
 	for _, let := range str {
-		if  let >= 'A' && let <= 'Z' || let >= 'a' && let <= 'z' {
+		if let >= 'A' && let <= 'Z' || let >= 'a' && let <= 'z' {
 			count++
 		}
 	}
