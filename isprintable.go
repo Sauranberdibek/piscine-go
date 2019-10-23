152dad329d8fd2541337b2445bbf7ffc18750444
Sauranberdibek <ms.sauran@gmail.com>
2019-10-23 21:58:09 +0600

    quest5-665

---
+++ isprintable.go
@@ -0,0 +1,16 @@
+ackage piscine
+
+func IsPrintable(str string) bool {
+	r := []rune(str)
+	ln := 0
+	for i := range r {
+		ln = i
+	}
+
+	for i := 0; i <= ln; i++ {
+		if r[i] < 32 {
+			return false
+		}
+	}
+	return true
+}
